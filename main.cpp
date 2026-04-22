9a3a87bf78beaf1ccac0d3f43a5091bedef2b012
ryouKenHou <k025g0112@g.neec.ac.jp>
2026-04-22 14:27:16 +0900

    CG2_00_04_EX: log file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <Windows.h>
 #include <cstdint>
 #include "ConvertString.hpp"
+#include <filesystem>
+#include <fstream>
+#include <chrono>
+
+void Log(std::ostream& os, const std::string& message) {
+	os << message <<std::endl;
+	OutputDebugStringA(message.c_str());
+}
+
+void Log(std::ostream& os, const std::wstring& message) {
+	Log(os, ConvertString(message));
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	switch (msg) {
@@ -14,7 +26,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 
 
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	Log("Hello, DirectX!\n");
+	std::filesystem::create_directories("logs");
+
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds> nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(), nowSeconds };
+	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
+	std::string logFilePath = std::string("logs/") + dateString + ".log";
+	std::ofstream logStream(logFilePath);
+
+
+	//Log("Hello, DirectX!\n");
+	Log(logStream, "Hello, DirectX!");
 
 	WNDCLASS wc{};
 
