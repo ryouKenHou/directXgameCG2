eaa45c0728d47b8930fbf0a1fc8393c05b2bb8b3
ryouKenHou <k025g0112@g.neec.ac.jp>
2026-05-20 19:16:32 +0900

    CG2_03_00: texture

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,18 +1,24 @@
+#include "Object3d.hlsli"
+
 struct Material
 {
     float32_t4 color;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
 
 struct PixelShaderOuput
 {
     float32_t4 color : SV_TARGET0;
 };
 
-PixelShaderOuput main()
+PixelShaderOuput main(VertexShaderOutput input)
 {
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    
     PixelShaderOuput output;
-    output.color = gMaterial.color;
+    output.color = gMaterial.color * textureColor;
     return output;
 }
